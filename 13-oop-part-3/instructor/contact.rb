385d384160ff047c647c0930dbf9b3296a868439
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2018-11-21 11:58:48 -0500

    [13-oop-part-3] Class functionality of the Contact class

--- 13-oop-part-3/instructor/contact.rb
+++ 13-oop-part-3/instructor/contact.rb
@@ -1,10 +1,32 @@
 class Contact
 
-  def initialize(first_name, last_name, email, notes)
+  @@list = []
+  @@next_id = 1000
+
+  def initialize(first_name, last_name, email, notes = 'N/A')
     @first_name = first_name
     @last_name = last_name
     @email = email
     @notes = notes
+
+    @id = nil
+  end
+
+  def self.all
+    @@list
+  end
+
+  def self.create(first_name, last_name, email, notes = 'N/A')
+    new_contact = Contact.new(first_name, last_name, email, notes)
+    new_contact.save
+    new_contact
+  end
+
+  def self.find(id)
+    # Left to the student as an exercise
+    # Iterate through the entire list of contacts
+    # Ask which one has that id
+    # Return that instance
   end
 
   # Reader
@@ -46,6 +68,12 @@ class Contact
   # The other through the reader
   def full_name
     "#{ @first_name } #{ last_name }"
+  end
+
+  def save
+    @id = @@next_id
+    @@next_id += 1
+    @@list << self
   end
 
 end
--- 13-oop-part-3/instructor/main.rb
+++ 13-oop-part-3/instructor/main.rb
@@ -3,15 +3,38 @@ require_relative 'contact'
 clark = Contact.new('Clark', 'Kent',
                     'clark@dailyplanet.com',
                     'Goes into the phone booth often')
+clark.save
 
 bruce = Contact.new('Bruce', 'Wayne',
                     'bruce@wayneenterprises.com',
                     "Doesn't smile very much")
+bruce.save
 
 diana = Contact.new('Diana', 'Prince',
-                    'diana@amazon.com',
-                    'N/A')
+                    'diana@amazon.com')
+diana.save
 
 puts clark.first_name
+
 puts bruce.last_name
+puts bruce.notes
+
 puts diana.full_name
+puts diana.notes
+
+puts Contact.all.length # 0
+
+puts Contact.all.length # 3
+
+barry = Contact.create('Barry', 'Allen', 'barry@csi.com')
+puts barry.full_name
+puts Contact.all.length # 4
+
+
+Contact.create('Dave', 'Wong', '')
+Contact.create('Dave', 'Wong', '')
+Contact.create('Dave', 'Wong', '')
+
+puts Contact.all.length # 7
+
+c = Contact.find(1005)

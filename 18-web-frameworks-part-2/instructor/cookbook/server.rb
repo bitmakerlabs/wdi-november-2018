ddb0a442d33f29d2e07e2968092a67b362168a51
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2018-11-29 11:06:08 -0500

    [18-web-frameworks-part-2] Show list of recipes on the recipe index

--- 18-web-frameworks-part-2/instructor/cookbook/server.rb
+++ 18-web-frameworks-part-2/instructor/cookbook/server.rb
@@ -21,5 +21,13 @@ end
 
 get '/recipes' do
   @title = 'Recipes'
+
+  # Notice that the keys are integers
+  @recipes = {
+    1 => 'Avocado Sushi',
+    2 => 'BBQ Ribs',
+    3 => 'Gluten-Free Cookies'
+  }
+
   erb :recipes
 end

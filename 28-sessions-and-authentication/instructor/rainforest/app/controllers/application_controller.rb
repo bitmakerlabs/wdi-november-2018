0308ad20321847127ecbc1c8633b223f5178344b
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2018-12-12 12:37:20 -0500

    [28-sessions-and-authn] Implement current_user helper method

--- 28-sessions-and-authentication/instructor/rainforest/app/controllers/application_controller.rb
+++ 28-sessions-and-authentication/instructor/rainforest/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user
+
+private
+
+  def current_user
+    user_id = session[:user_id]
+    user_id && User.find(user_id)
+  end
+
 end

9214394318830e0e87e53171b179c2989e0031c0
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2018-12-12 12:11:20 -0500

    [28-sessions-and-authn] Add logged in /logged out links to the layout

--- 28-sessions-and-authentication/instructor/rainforest/app/controllers/sessions_controller.rb
+++ 28-sessions-and-authentication/instructor/rainforest/app/controllers/sessions_controller.rb
@@ -3,6 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    submitted_email = params[:session][:email]
+    submitted_password = params[:session][:password]
+
+    u = User.find_by(email: submitted_email)
+
+    if u && u.authenticate(submitted_password)
+      # flash: "Access Granted!"
+      redirect_to root_url
+    else
+      # flash: 'Email or Password invalid!'
+      render :new
+    end
   end
 
   def destroy

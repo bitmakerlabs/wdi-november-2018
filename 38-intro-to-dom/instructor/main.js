98be956a782352a20b011e38257fb2d223c76ed2
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2019-01-02 12:05:07 -0500

    In class solution for Intro to DOM

--- 38-intro-to-dom/instructor/main.js
+++ 38-intro-to-dom/instructor/main.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(e){
   // CODE GOES HERE
+  var h1 = document.querySelector('h1');
+  h1.innerText = "I'm in a file";
+
+  var ul = document.querySelector('ul');
+  var li;
+  for (var i = 0; i < 10000; i++) {
+    li = document.createElement('li');
+    li.innerText = "Some text";
+    li.className = "item";
+    ul.appendChild(li);
+  }
 })

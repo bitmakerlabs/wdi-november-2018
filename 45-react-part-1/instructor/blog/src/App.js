a71a01c7320aa150c9e953ebfc207aec4b91276e
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2019-01-11 10:52:53 -0500

    [45-react-part-1] Intro to props

--- 45-react-part-1/instructor/blog/src/App.js
+++ 45-react-part-1/instructor/blog/src/App.js
@@ -5,21 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="post">
+        <h1>{ this.props.post.title }</h1>
+        <h2>by: { this.props.post.author }</h2>
+        <p>{ this.props.post.body }</p>
+
+        <h3>Comments</h3>
+        <p>{ this.props.post.comments[0] }</p>
+        <p>{ this.props.post.comments[1] }</p>
+        <p>{ this.props.post.comments[2] }</p>
       </div>
     );
   }

a5e98f6e3e734c858babac46c45d2b077dff5e73
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2019-01-14 12:02:21 -0500

    Use functional components

--- 46-react-part-2/instructor/todo/src/ListItem.js
+++ 46-react-part-2/instructor/todo/src/ListItem.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+//
+// class ListItem extends Component {
+//   render() {
+//     return (
+//       <li>{ this.props.item }</li>
+//     )
+//   }
+// }
+//
+// export default ListItem
 
-class ListItem extends Component {
-  render() {
-    return (
-      <li>{ this.props.item }</li>
-    )
-  }
-}
+import React from 'react'
+
+// Functional Component
+const ListItem = props =>
+  <li>{ props.item }</li>
 
 export default ListItem
--- 46-react-part-2/instructor/todo/src/MyList.js
+++ 46-react-part-2/instructor/todo/src/MyList.js
@@ -1,22 +1,43 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+//
+// import ListItem from './ListItem'
+//
+// class MyList extends Component {
+//   render() {
+//     const allListItems = this.props.theList.map( (item, index) =>
+//       <ListItem key={ index } item={ item } />
+//     )
+//
+//     return (
+//       <div>
+//         <h1>Things I should stop procrastinating on:</h1>
+//         <ul>
+//           { allListItems }
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+//
+// export default MyList
 
+import React from 'react'
 import ListItem from './ListItem'
 
-class MyList extends Component {
-  render() {
-    const allListItems = this.props.theList.map( (item, index) =>
-      <ListItem key={ index } item={ item } />
-    )
+// Functional version of MyList
+const MyList = props => {
+  const allListItems = props.theList.map( (item, index) =>
+    <ListItem key={ index } item={ item } />
+  )
 
-    return (
-      <div>
-        <h1>Things I should stop procrastinating on:</h1>
-        <ul>
-          { allListItems }
-        </ul>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>Things I should stop procrastinating on:</h1>
+      <ul>
+        { allListItems }
+      </ul>
+    </div>
+  )
 }
 
 export default MyList

2b19f634ce04346d4e695ed215fb4af4d4ead074
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2019-01-15 11:48:12 -0500

    [47-react-part-3] Add support for all operations

--- 47-react-part-3/instructor/calculator/src/Calculator.js
+++ 47-react-part-3/instructor/calculator/src/Calculator.js
@@ -4,35 +4,68 @@ class Calculator extends Component {
   state = {
     val1: 0,
     val2: 0,
+    operation: "+",
     result: 0
   }
 
-  handleChange = (event) => {
-    // // Easy Solution
-    // const val1 = document.getElementById("val1").value
-    // const val2 = document.getElementById("val2").value
-    // const result = (parseInt(val1) || 0) + (parseInt(val2) || 0)
-    // this.setState({ result: result })
+  calculate = () => {
+    const val1 = this.state.val1
+    const val2 = this.state.val2
+    let result = 0
 
+    switch(this.state.operation) {
+      case "+": {
+        result = val1 + val2
+        break
+      }
+      case "-": {
+        result = val1 - val2
+        break
+      }
+      case "*": {
+        result = val1 * val2
+        break
+      }
+      case "/": {
+        result = val1 / val2
+        break
+      }
+      default: {
+        console.log("Invalid operation!")
+        break
+      }
+    }
+
+    this.setState({ result })
+  }
+
+  handleValChange = (event) => {
     // Advanced solution with a "computed property name"
     this.setState({
       [event.target.name]: (parseInt(event.target.value) || 0)
-    }, () => {
-      this.setState({
-        result: this.state.val1 + this.state.val2
-      })
-    })
+    }, this.calculate)
+  }
+
+  handleOperationChange = (event) => {
+    this.setState({
+      operation: event.target.value
+    }, this.calculate)
   }
 
   render() {
     return (
       <div className="container">
-        <h1>Add with React</h1>
+        <h1>Calculate with React</h1>
 
         <div className="add">
-          <input name="val1" type="text" onChange={ this.handleChange } />
-          <span>+</span>
-          <input name="val2" type="text" onChange={ this.handleChange } />
+          <input name="val1" type="text" onChange={ this.handleValChange } />
+          <select name="operation" onChange={ this.handleOperationChange } >
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+          </select>
+          <input name="val2" type="text" onChange={ this.handleValChange } />
           <span>=</span>
           <h3>{ this.state.result }</h3>
         </div>

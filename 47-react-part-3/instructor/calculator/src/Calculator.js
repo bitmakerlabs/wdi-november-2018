deddce03c322d4d28c6e91fd5c33aa8d1965ba08
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2019-01-15 11:24:27 -0500

    [47-react-part-3] Easy solution for addition

--- 47-react-part-3/instructor/calculator/src/Calculator.js
+++ 47-react-part-3/instructor/calculator/src/Calculator.js
@@ -5,15 +5,23 @@ class Calculator extends Component {
     result: 0
   }
 
+  handleChange = (event) => {
+    // Easy Solution
+    const val1 = document.getElementById("val1").value
+    const val2 = document.getElementById("val2").value
+    const result = (parseInt(val1) || 0) + (parseInt(val2) || 0)
+    this.setState({ result: result })
+  }
+
   render() {
     return (
       <div className="container">
         <h1>Add with React</h1>
 
         <div className="add">
-          <input type="text" />
+          <input id="val1" type="text" onChange={ this.handleChange } />
           <span>+</span>
-          <input type="text" />
+          <input id="val2" type="text" onChange={ this.handleChange } />
           <span>=</span>
           <h3>{ this.state.result }</h3>
         </div>

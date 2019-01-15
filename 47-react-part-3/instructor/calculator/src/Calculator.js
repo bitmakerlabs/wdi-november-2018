ab9000aecedd2a2b641ace3dfc4e4ce2284a2cab
gurjant31 <bitmakerlabs@Bitmaker-2.local>
2019-01-15 11:34:06 -0500

    [47-react-part-3] Handle addition with computed propery name

--- 47-react-part-3/instructor/calculator/src/Calculator.js
+++ 47-react-part-3/instructor/calculator/src/Calculator.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react'
 
 class Calculator extends Component {
   state = {
+    val1: 0,
+    val2: 0,
     result: 0
   }
 
   handleChange = (event) => {
-    // Easy Solution
-    const val1 = document.getElementById("val1").value
-    const val2 = document.getElementById("val2").value
-    const result = (parseInt(val1) || 0) + (parseInt(val2) || 0)
-    this.setState({ result: result })
+    // // Easy Solution
+    // const val1 = document.getElementById("val1").value
+    // const val2 = document.getElementById("val2").value
+    // const result = (parseInt(val1) || 0) + (parseInt(val2) || 0)
+    // this.setState({ result: result })
+
+    // Advanced solution with a "computed property name"
+    this.setState({
+      [event.target.name]: (parseInt(event.target.value) || 0)
+    }, () => {
+      this.setState({
+        result: this.state.val1 + this.state.val2
+      })
+    })
   }
 
   render() {
@@ -19,9 +30,9 @@ class Calculator extends Component {
         <h1>Add with React</h1>
 
         <div className="add">
-          <input id="val1" type="text" onChange={ this.handleChange } />
+          <input name="val1" type="text" onChange={ this.handleChange } />
           <span>+</span>
-          <input id="val2" type="text" onChange={ this.handleChange } />
+          <input name="val2" type="text" onChange={ this.handleChange } />
           <span>=</span>
           <h3>{ this.state.result }</h3>
         </div>
